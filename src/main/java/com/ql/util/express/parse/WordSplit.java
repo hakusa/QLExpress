a14f2e1fce2f5127158bb91a587549d6e7156278
meihaoshidai <845611259@qq.com>
2020-07-27 21:18:50 +0800

    Revert "修复反斜杠\识别的bug"
    This reverts commit f53013c39e2759d47b4e219bddd59ddc11ba4b06.

--- src/main/java/com/ql/util/express/parse/WordSplit.java
+++ src/main/java/com/ql/util/express/parse/WordSplit.java
@@ -40,7 +40,7 @@ public class WordSplit
 	       if (c=='"' || c=='\''){//字符串处理        
 	     	int index = str.indexOf(c,i + 1);
 	     	//处理字符串中的”问题
-	         while(index >0 && str.charAt(index - 1) =='\\' && str.charAt(index - 2) !='\\'){
+	         while(index >0 && str.charAt(index - 1) =='\\'){
 	         	index = str.indexOf(c,index + 1);
 	         }
 	         if (index < 0)
